130544bdf40d4337ef5deed8d6b37ada46b30492
Lokesh_Kudipudi <lokeshkudipudi2006@gmail.com>
2025-09-09 15:02:28 +0530

    t4-2

--- T4/1d-openMP.c
+++ T4/1d-openMP.c
@@ -22,15 +22,19 @@ void matrix_mul(int A[][N], int X[], int Y[], int n)
 int main()
 {
   int A[N][N], X[N], Y[N];
+  int numberA = 0;
+  int numberX = 1;
 
   // Initialize matrices with some values instead of user input
   for (int i = 0; i < N; i++)
   {
     for (int j = 0; j < N; j++)
     {
-      A[i][j] = i + j; // Simple initialization pattern
+      A[i][j] = numberA; // Simple initialization pattern
+      numberA++;
     }
-    X[i] = i; // Initialize vector X
+    X[i] = numberX; // Initialize vector X
+    numberX++;
     Y[i] = 0; // Initialize result vector Y
   }
 
