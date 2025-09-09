0bccf0e6e1dd5b692c818437c85d777288477bcd
Lokesh Kudipudi <lokeshkudipudi2006@gmail.com>
2025-09-09 09:48:14 +0000

    t4-2

--- T4/1d-openMP.c
+++ T4/1d-openMP.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
-#define N 10
 
-void matrix_mul(int A[][N], int X[], int Y[], int n)
+void matrix_mul(int **A, int *X, int *Y, int n)
 {
 
 #pragma omp parallel
   {
     int threadID = omp_get_thread_num();
     int numThreads = omp_get_num_threads();
-    int offset = n / numThreads; // Ceiling division
-    for (int i = threadID * offset; i < threadID * offset + offset; i++)
+    int offset = n / numThreads + (threadID < n % numThreads ? 1 : 0);
+    int start = threadID * (n / numThreads) + (threadID < n % numThreads ? threadID : n % numThreads);
+    int end = start + offset;
+    printf(" Thread %d processing %d to %d ", threadID, start, end-1);
+    for (int i = start; i < end; i++)
     {
       Y[i] = 0;
       for (int k = 0; k < n; k++)
@@ -19,44 +22,48 @@ void matrix_mul(int A[][N], int X[], int Y[], int n)
   }
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-  int A[N][N], X[N], Y[N];
-  int numberA = 0;
+  if (argc < 2) {
+    printf("Usage: %s <N>\n", argv[0]);
+    return 1;
+  }
+  int N = atoi(argv[1]);
+  int num_threads = atoi(argv[2]);
+  int **A = malloc(N * sizeof(int*));
+  int *X = malloc(N * sizeof(int));
+  int *Y = malloc(N * sizeof(int));
+  int numberA = 1;
   int numberX = 1;
-
-  // Initialize matrices with some values instead of user input
-  for (int i = 0; i < N; i++)
-  {
-    for (int j = 0; j < N; j++)
-    {
-      A[i][j] = numberA; // Simple initialization pattern
+  for (int i = 0; i < N; i++) {
+    A[i] = malloc(N * sizeof(int));
+    for (int j = 0; j < N; j++) {
+      A[i][j] = numberA;
       numberA++;
     }
-    X[i] = numberX; // Initialize vector X
+    X[i] = numberX;
     numberX++;
-    Y[i] = 0; // Initialize result vector Y
+    Y[i] = 0;
   }
-
   printf("Matrix size: %dx%d\n", N, N);
-
-  // Get number of available threads
   int max_threads;
 #pragma omp parallel
   {
-#pragma omp master
+  #pragma omp master
     {
       max_threads = omp_get_num_threads();
       printf("Number of threads: %d\n", max_threads);
     }
   }
-
-#pragma omp num_threads(2)
+#pragma omp num_threads(num_threads)
   matrix_mul(A, X, Y, N);
-
   printf("Resultant Matrix Y:\n");
   for (int i = 0; i < N; i++)
     printf("%d ", Y[i]);
   printf("\n");
+  for (int i = 0; i < N; i++) free(A[i]);
+  free(A);
+  free(X);
+  free(Y);
   return 0;
 }
